c3e58012b6e9101ba9980800f1ab33f1aca0a8c7
Bogdan Rudnevskii <bogdan.rudnevskii@fliq.fi>
2025-02-12 00:32:15 +0200

    init commit for axum project

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::net::{SocketAddr, TcpListener};
+use axum::http::header::SERVER;
+use axum::response::Html;
+use axum::{Router, ServiceExt};
+use axum::routing::get;
+use axum::serve::Serve;
+
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("<h1>Hello, World!</h1>") }),
+    );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, routes_hello.into_make_service()).await.unwrap();
+}
