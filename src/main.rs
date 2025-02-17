730e79a8b1d52d17c1a4a1ea73937c441764a730
Segfaulter <brudnevskij@users.noreply.github.com>
2025-02-17 23:33:07 +0200

    moved hello handler to separated func

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
-use std::net::{SocketAddr, TcpListener};
-use axum::http::header::SERVER;
-use axum::response::Html;
+use std::net::{SocketAddr,};
+use axum::response::{Html, IntoResponse};
 use axum::{Router, ServiceExt};
 use axum::routing::get;
-use axum::serve::Serve;
 
 
 #[tokio::main]
 async fn main() {
     let routes_hello = Router::new().route(
         "/hello",
-        get(|| async { Html("<h1>Hello, World!</h1>") }),
+        get(handler_hello),
     );
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("listening on {}", addr);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, routes_hello.into_make_service()).await.unwrap();
+}
+
+async fn handler_hello() -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+    Html("<h1>Hello</h1>")
 }
