a3264c4b2e2eeb031e9aa177c289d625839604ab
Segfaulter <brudnevskij@users.noreply.github.com>
2025-02-18 00:19:52 +0200

    Added path serialization and test handler_hello2

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,37 @@
-use std::net::{SocketAddr,};
+use axum::extract::{Path, Query};
 use axum::response::{Html, IntoResponse};
-use axum::{Router, ServiceExt};
-use axum::extract::Query;
 use axum::routing::get;
+use axum::{Router, ServiceExt};
 use serde::Deserialize;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(handler_hello),
-    );
+    let routes_hello = Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/{name}", get(handler_hello2));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("listening on {}", addr);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    axum::serve(listener, routes_hello.into_make_service()).await.unwrap();
+    axum::serve(listener, routes_hello.into_make_service())
+        .await
+        .unwrap();
 }
 
 #[derive(Debug, Deserialize)]
 struct HelloParams {
-    name: Option<String>
+    name: Option<String>,
 }
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello", "HANDLER");
 
     let name = params.name.as_deref().unwrap_or("World");
+    Html(format!("<h1>Hello, {name}</h1>"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+
     Html(format!("<h1>Hello, {name}</h1>"))
 }
